ac5eac44cf088c183a445b3c4c69a517cb101c04
Ian Houghton <sales@redmorello.com>
2019-10-08 16:21:49 +0100

    Updated version number

--- src/Our.Umbraco.DocTypePicker/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.DocTypePicker/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ef873cb9-db5d-42a8-a68f-77d528c6968b")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
